e0e4b490491fa1eef83680fa2b5d594d80897aca
Titus Wormer <tituswormer@gmail.com>
2023-01-18 19:56:29 +0100

    Add tests for exposed identifiers

--- test/index.js
+++ test/index.js
@@ -3,3 +3,15 @@ import './node.js'
 import './children.js'
 import './root.js'
 /* eslint-enable import/no-unassigned-import */
+
+import nodeAssert from 'node:assert/strict'
+import test from 'node:test'
+import * as mod from '../index.js'
+
+test('api', () => {
+  nodeAssert.deepEqual(
+    Object.keys(mod).sort(),
+    ['_void', 'assert', 'literal', 'parent', 'wrap'],
+    'should expose the public api'
+  )
+})
